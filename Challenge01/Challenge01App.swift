9c1c4a9a6c999c14bf6a3bc00e6b72a579d9c1a0
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-02-09 14:00:04 +0330

    Initial Commit

---
+++ Challenge01/Challenge01App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge01App.swift
+//  Challenge01
+//
+//  Created by Sothesom on 21/11/1403.
+//
+
+import SwiftUI
+
+@main
+struct Challenge01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge01/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge01
+//
+//  Created by Sothesom on 21/11/1403.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
